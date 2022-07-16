beb272ebf505aeed687c29d5870572549dda3a41
Jonathan Lawrence <sorosune@gmail.com>
2022-07-16 12:19:35 -0400

    Reparent new player BP to the cpp pawn class.

--- Source/Core/Actors/ImpulseGJ22Player.h
+++ Source/Core/Actors/ImpulseGJ22Player.h
@@ -7,7 +7,7 @@
 #include "ImpulseGJ22Player.generated.h"
 
 UCLASS(config=Game)
-class AImpulseGJ22Player : public ACharacter
+class AImpulseGJ22Player : public APawn
 {
 	GENERATED_BODY()
 
@@ -20,12 +20,6 @@ public:
 	// Blueprint Variables
 
 	// Getters
-
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 	// Setters
 
@@ -58,47 +52,11 @@ protected:
 
 	// Internal Variables
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
-
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
-	float BaseTurnRate;
-
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
-	float BaseLookUpRate;
-
 	// Internal Virtual Functions
 
 	// Internal Overrides
 
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 	// Internal Regular Functions
-
-	/** Called for forwards/backward input */
-	void MoveForward(float Value);
-
-	/** Called for side to side input */
-	void MoveRight(float Value);
-
-	/**
-	 * Called via input to turn at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
 
 	// Internal Events and Implementations 
 
